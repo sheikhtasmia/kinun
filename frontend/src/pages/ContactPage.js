bdfd3705f6b0951dd8afb06cb048fb611ce9382a
Sheikh Tasmia <200295689+sheikhtasmia@users.noreply.github.com>
2025-10-30 02:56:41 +0600

    Create ContactPage.js

---
+++ frontend/src/pages/ContactPage.js
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+    return (
+        <div>
+            <h1 className="font-bold text-2xl mb-4">Contact Us</h1>
+            <p>Email: support@kinun.com</p>
+            <p>Phone: +1 234 567 890</p>
+        </div>
+    );
+}
