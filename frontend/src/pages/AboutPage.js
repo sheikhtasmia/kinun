b4cf2710945fbb400cce2e6d36e756d6df124298
Sheikh Tasmia <200295689+sheikhtasmia@users.noreply.github.com>
2025-10-30 02:55:37 +0600

    Create AboutPage.js

---
+++ frontend/src/pages/AboutPage.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1 className="font-bold text-2xl mb-4">About Kinun Dot Com</h1>
+            <p>Kinun Dot Com is your one-stop shop for electronics, clothing, accessories, home, beauty, kitchen, kids, stationery, grocery, and sports products. We bring you the best affiliate deals!</p>
+        </div>
+    );
+}
